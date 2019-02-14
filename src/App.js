ab1c4d8608ba8b19d9650d4dc520748c3bf167fc
wcolts2000 <wcolts2000@gmail.com>
2019-02-13 23:31:22 -0800

    added highlights area

--- src/App.js
+++ src/App.js
@@ -1,19 +1,18 @@
 import React, { Component } from "react";
 import Featured from "./components/featured/index";
 import Header from "./components/header_footer/Header";
+import Highlights from "./components/highlights/index";
 import VenueInfo from "./components/venueInfo/index";
 import "./resources/styles.css";
 
 class App extends Component {
   render() {
     return (
-      <div
-        className="App"
-        style={{ height: "1500px", background: "cornflowerBlue" }}
-      >
+      <div className="App">
         <Header />
         <Featured />
         <VenueInfo />
+        <Highlights />
       </div>
     );
   }
---
+++ src/components/highlights/Description.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Fade from 'react-reveal/Fade';
+
+function Description() {
+  return (
+    <Fade>
+      <div className="center_wrapper">
+        <h2>Highlights</h2>
+        <div className="highlight_description">
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Assumenda, labore. Dolore mollitia quidem aliquam optio consequuntur, aspernatur omnis esse, repellat id ipsa autem. Eaque ipsum esse aspernatur et minus quis excepturi, quis necessitatibus in adipisci eaque nulla eveniet.
+        </div>
+      </div>
+    </Fade>
+  )
+}
+
+export default Description
